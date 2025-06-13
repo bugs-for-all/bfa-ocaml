688ab4b0654899f1d36d7ff6ddcc6e82b71513eb
N1ark <opale.sjostedt@gmail.com>
2025-06-14 08:55:05 +0900

    try using charon layout (hell)

--- soteria-rust/lib/interp.ml
+++ soteria-rust/lib/interp.ml
@@ -588,6 +588,7 @@ module Make (Heap : Heap_intf.S) = struct
             let discr = Typed.int_z discriminant.value in
             Result.ok (Base discr, state)
         | var :: _ ->
+            (* FIXME: this type is wrong *)
             let int_ty = var.discriminant.int_ty in
             let layout = Layout.of_variant var in
             let discr_ofs = Typed.int @@ Array.get layout.members_ofs 0 in
